4be3b9abf3c51dd4d11e2b7ba7ac5be255c6bd03
prodbuild <shamajit@gmail.com>
2015-12-28 11:31:46 +0530

    Log4Net

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Log4NetDemoApplication
+{
+    class Program
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger("Log4NetDemoApplication");
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                logger.Debug("Hello");
+
+                logger.Error("Exceptino occurred", new Exception("Serious Exception"));
+
+                logger.Info("Hello nothing happened.");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
